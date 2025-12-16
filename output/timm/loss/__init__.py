ac693a68ed2d29829a7b7be08cc7dec9703b8ec0
bellachiao666 <897246307@qq.com>
2025-12-16 10:26:09 +0800

    Revert "修复不能转换的bug"
    This reverts commit cfa316bbfdb4bae9517d50df66a839802e57dd6f.

--- output/timm/loss/__init__.py
+++
@@ -1,9 +0,0 @@
-import mindspore as ms
-import mindspore.nn as msnn
-import mindspore.ops as msops
-import mindspore.mint as mint
-from mindspore.mint import nn, ops
-from .asymmetric_loss import AsymmetricLossMultiLabel, AsymmetricLossSingleLabel
-from .binary_cross_entropy import BinaryCrossEntropy
-from .cross_entropy import LabelSmoothingCrossEntropy, SoftTargetCrossEntropy
-from .jsd import JsdCrossEntropy
